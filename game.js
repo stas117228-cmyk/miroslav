90873b825a985c182b3f3a0c828c385a6eb84b6d
stas117228-cmyk <stas117228@gmail.com>
2025-11-01 05:28:37 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,2 @@
+﻿const socket = io();
+console.log('Game script loaded');
---
+++ server.js
@@ -0,0 +1,19 @@
+﻿const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 10000;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(Server running on port );
+});
