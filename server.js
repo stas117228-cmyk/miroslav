583006f751db2d7dad50f3d9c096eab989b73083
stas117228-cmyk <stas117228@gmail.com>
2025-11-01 06:05:05 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+﻿const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('answer', (data) => {
+        io.emit('new-answer', data);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(Server running on port ));
