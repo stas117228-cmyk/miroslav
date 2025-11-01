1e365fa612b526e2d3fd8f8c326014825df3d2cd
stas117228-cmyk <stas117228@gmail.com>
2025-11-01 05:59:20 +0300

    Add files via upload

---
+++ client.js
@@ -0,0 +1,25 @@
+﻿const socket = io();
+const roomId = prompt('Enter room ID:');
+const playerName = prompt('Enter your name:');
+socket.emit('joinRoom', roomId, playerName);
+
+const playersDiv = document.getElementById('players');
+const answersDiv = document.getElementById('answers');
+const answerInput = document.getElementById('answerInput');
+const submitBtn = document.getElementById('submitBtn');
+
+submitBtn.addEventListener('click', () => {
+  const answer = answerInput.value.trim();
+  if(answer) {
+    socket.emit('submitAnswer', roomId, answer);
+    answerInput.value = '';
+  }
+});
+
+socket.on('updatePlayers', (players) => {
+  playersDiv.innerHTML = '<h3>Players:</h3>' + players.map(p => p.name).join('<br>');
+});
+
+socket.on('updateAnswers', (answers) => {
+  answersDiv.innerHTML = '<h3>Answers:</h3>' + answers.map(a => a.answer).join('<br>');
+});
